a65f177c20ffdd632c9bfcc72b141db8a8546b2d
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-01-19 13:03:00 +0100

    default

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,19 +1,3 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -22,7 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= $this->fetch('title') ?>
+
+    <title>Mediflex</title>
 
     <?= $this->Html->css(['style', 'zebra', 'dr-framework', 'revslider', 'jquery.bxslider', 'responsive' ]) ?>
     <link rel="stylesheet" type="text/css" href="/css/fullwidth.html" media="screen" />
