01b1821fa673090236b29b42a4dca3ae12740ac9
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-01-19 12:27:30 +0100

    debug 9

--- templates/Mediflex/index.php
+++ templates/Mediflex/index.php
@@ -1,39 +1,36 @@
 <!-- Slider -->
 <div class="slider" >
-		<div class="flexslider">
-		  <ul class="slides">
-		    <li>
-		      <img src="images/slider-img.jpg" />
-		    </li>
-			<li>
-			  <img src="images/slider-img.jpg" />
-			</li>
-		  </ul>
-		</div>
+    <div class="flexslider">
+        <ul class="slides">
+            <li>
+                <img src="/images/slider-img.jpg" />
+            </li>
+            <li>
+                <img src="/images/slider-img.jpg" />
+            </li>
+        </ul>
+    </div>
 
 
 	<!-- Book Apointment -->
 	<div class="book-form">
 		<div class="inner-form">
 
-		<h4><img src="images/calendar.png" alt="">Book an Appointment</h4>
+		<h4>Payer Une Facture</h4>
 		<form id="contact-form2" action="#">
 			<div class="inputs">
 				<input name="name" id="name" type="text" data-value="Full Name">
 				<input name="mail" id="mail" type="text" data-value="E-mail Address">
-				<input name="phone" id="phone" type="text" data-value="Phone Number">
-	       		<input name="date" id="date" type="text" data-value="dd/mm/yyyy 00:00">
 	        </div>
 
-			<input id="submit_contact2" type="submit" value="MAKE APPOINTMENT">
-			<div id="msg2" class="message"></div>
+			<input id="submit_contact2" type="submit" value="Payer">
 		</form>
 
 		</div>
 	</div>
 	<!-- End Book Apointment -->
-	</div>
-	<!-- End SLider -->
+</div>
+<!-- End SLider -->
 
 
 
